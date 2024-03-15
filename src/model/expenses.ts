bfd89802daf4158cf3ea777d1c024ba3e1fd7b4b
aversii <lucasaversi@gmail.com>
2024-03-14 21:42:17 -0300

    feat: expenses model created

---
+++ src/model/expenses.ts
@@ -0,0 +1,24 @@
+export class Expenses {
+    constructor(
+      private id: string,
+      private value: number,
+      private userId: string,
+      private createdAt: number = Date.now()
+    ) {}
+  
+    getId(): string {
+      return this.id;
+    }
+  
+    getValue(): number {
+      return this.value;
+    }
+  
+    getUserId(): string {
+      return this.userId;
+    }
+  
+    getCreatedAt(): number {
+      return this.createdAt;
+    }
+  }
